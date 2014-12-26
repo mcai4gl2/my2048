af837271851eb3d7cb3c80b3649b325cc5fee0f9
Geng Li <ligeng0420@gmail.com>
2014-12-26 21:11:03 +0000

    Copied in all files to play with

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,35 @@
 var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	prefix = require('gulp-autoprefixer'),
+	react = require('gulp-react'),
 	qunit = require('gulp-qunit');
+
+var styles = 'src/*.scss';
+var scripts = 'src/*.js';
+
+gulp.task('styles', function () {
+  return gulp.src(styles)
+    .pipe(sass())
+    .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'ie 7'))
+    .pipe(gulp.dest('built'));
+});
+
+gulp.task('jsx', function () {
+  return gulp.src(scripts)
+    .pipe(react())
+    .pipe(gulp.dest('built'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(styles, ['styles']);
+  gulp.watch(scripts, ['jsx']);
+});
 
 gulp.task('test', function() {
 	return gulp.src('./tests/unit/tile.html')
 		.pipe(qunit());
 });
+
+gulp.task('default', ['test', 'watch', 'styles', 'jsx']);
+
+gulp.task('build', ['test', 'styles', 'jsx']);
--- src/tile.js
+++ src/tile.js
@@ -167,3 +167,16 @@ Board.prototype.hasLost = function () {
   }
   return !canMove;
 };
+
+var rotateLeft = function (matrix) {
+  var rows = matrix.length;
+  var columns = matrix[0].length;
+  var res = [];
+  for (var row = 0; row < rows; ++row) {
+    res.push([]);
+    for (var column = 0; column < columns; ++column) {
+      res[row][column] = matrix[column][columns - row - 1];
+    }
+  }
+  return res;
+};
